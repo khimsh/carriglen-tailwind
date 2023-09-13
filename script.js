8219dea711154b62f0b4d391ac0337c8f914121f
khimsh <i.khimsh@gmail.com>
2023-09-13 10:45:38 +0400

    idk what i'm doing here

---
+++ script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    // autoplay: true,
+    // delay: 2000,
+    speed: 1000,
+    allowTouchMove: false,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-next',
+        prevEl: '.swiper-prev',
+    },
+})
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./*.{html,js}'],
+    theme: {
+        extend: {},
+        container: {
+            center: true,
+        },
+        fontFamily: {
+            montserrat: ['Montserrat', 'sans-serif'],
+            niconne: ['Niconne', 'cursive'],
+        },
+    },
+    plugins: [],
+}
